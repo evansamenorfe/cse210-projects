f348c31e99e8e8892e7363e32674ba3772f4f0c4
Evans Amenorfe <evansamenorfe@gmail.com>
2024-01-10 22:51:38 +0000

    Wrote the code to check the grade for students

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+       Console.Write("What is your grade in percentage? ");
+       String userInput = Console.ReadLine();
+       int grade = int.Parse(userInput);
+        string letter = "";
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if ( grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter ="D";
+        }
+
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade letter is {letter}");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulation, You have passed the class!!!!");
+        }
+        else
+        {
+            Console.WriteLine("Sorry you need to work harder to be able to pass this class.");
+        }
+
     }
 }
