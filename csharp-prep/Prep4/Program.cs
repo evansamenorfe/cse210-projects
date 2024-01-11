3532476ba873994d083314eef9d5d1f0c79806a3
Evans Amenorfe <evansamenorfe@gmail.com>
2024-01-11 13:59:13 +0000

    Created a list that adds and gets the average of numbers entered

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,56 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+
+        
+        
+        int numb = -1;
+        while (numb != 0)
+        {
+
+        
+
+            Console.Write("Enter a number, Type zero '0' to stop: ");
+            string inputs = Console.ReadLine();
+            numb = int.Parse(inputs);
+            
+
+            //Console.WriteLine(numbers.Count);
+
+            if (numb != 0)
+            {
+                numbers.Add(numb);
+            }
+
+            //Console.WriteLine(numbers);
+        }
+
+        //Sum
+
+        int sum = 0;
+        foreach (int digit in numbers)
+        {
+            sum += digit;
+        }
+        Console.WriteLine($"The sum is: {sum}");
+
+        //Average
+
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        //Maximum
+
+        int max = numbers[0];
+        foreach (int digit in numbers)
+        {
+            if (digit > max)
+            {
+                max = digit;
+            }
+        }
+
+        Console.WriteLine($"The maximum value is: {max}");
     }
 }
