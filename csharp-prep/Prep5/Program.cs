7ba69b980127ea685c53eded437dcce097f9a15a
Evans Amenorfe <evansamenorfe@gmail.com>
2024-01-11 14:18:28 +0000

    Created a programe for functions

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string userName = PromptUserName();
+        int userNumber = PromptUserNumber();
+        int square = SquareNumber(userNumber);
+        DisplayResult(userName, square);
+
+        static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the programe");
+        }
+
+        static string PromptUserName()
+        {
+            Console.Write("What is your name? ");
+            string userName = Console.ReadLine();
+            return userName;
+        }
+
+        static int PromptUserNumber()
+        {
+            Console.Write("What is your favourite number? ");
+            int userNumber = int.Parse(Console.ReadLine());
+            return userNumber;
+
+        }
+
+        static int SquareNumber(int userNumber)
+        {
+            int square = userNumber * userNumber;
+            return square;
+        }
+
+        static void DisplayResult(string userName, int square)
+        {
+            Console.WriteLine($"Hello {userName}, the square of your number is {square}.");
+        }
     }
 }
