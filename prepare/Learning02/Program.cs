b298d2f4db0b7a5113c21c7708a58e72139fbce0
Evans Amenorfe <evansamenorfe@gmail.com>
2024-01-16 09:32:13 +0000

    Completed the resume practice

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,31 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "Blow Chem Industry";
+        job1._jobTitle = "Laboratory Assistant";
+        job1._startYear = 2020;
+        job1._endYear = 2022;
+        
+
+        Job job2 = new Job();
+        job2._company = "Bloom";
+        job2._jobTitle = "Mentor";
+        job2._startYear = 2022;
+        job2._endYear = 2024;
+
+        Resume myResume = new Resume();
+        myResume._name = "Evans Amenorfe";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
+   
+    
 }
